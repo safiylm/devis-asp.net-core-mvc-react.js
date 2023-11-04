c4af81c9097b7fe04b6d5a72121f5e4c73a3361c
safiylm <121391404+safiylm@users.noreply.github.com>
2023-11-04 15:12:51 +0100

    liste devis frontend

--- ClientApp/src/components/ListeDevis/ListeDevis.js
+++ ClientApp/src/components/ListeDevis/ListeDevis.js
@@ -1,27 +1,71 @@
 ﻿import React, { useState } from 'react';
+import '../../styles/listedevis.css';
 
 const ListeDevis = () => {
-    const [produits, setProduit] = useState([]);
+    const [deviss, setDevis] = useState([]);
+    const [clients, setClients] = useState([]);
+    //const [auteurs, setAuteurs] = useState([]);
 
     fetch('http://localhost:44453/Devis/GetAll')
         .then((res) => res.json()  )
-        .then((data) => setProduit(data));
+        .then((data) => setDevis(data));
 
-    console.log(produits);
+    fetch('http://localhost:44453/Client/GetById?id=1')
+        .then((res) => res.json())
+        .then((data) => setClients(data));
+
+    //fetch('http://localhost:44453/Auteur/GetAll')
+    //    .then((res) => res.json())
+    //    .then((data) => setDevis(data));
+
+
     return (
 
-        <div>
+        <div className="div-liste-devis">
             <h1>Liste de Devis </h1>
-            {(produits.length > 0) ?
-                produits.map((item) => (
-                    <li key={item.id}>
 
-                        {JSON.stringify(item, null, 2)}
+            <table>
+                <thead>
+                    <tr>
+                        <th>N° de devis </th>
+                        <th>Client</th>
+                        <th>Montant HT</th>
+                        <th>Montant TTC</th>
+                        <th>Date du devis</th>
+                        <th>Statut</th>
 
-                    </li>
-                )) :
-                <div>Loading ...</div>
-            }
+                    </tr>
+                </thead>
+                <tbody>
+                   
+                    {deviss.map( devis => (
+                  <tr  key={devis.id}>
+                            <td>{devis.id}</td>
+
+                            {clients.map(client => (
+                                <td key={client.id}> {client.prenom} {client.nom} </td>
+                            ))}
+                         
+                            <td>
+                               200$
+                            </td>
+
+                            <td>
+                                220$
+                            </td>
+                            <td>
+                                <a> {devis.dateCreation} </a>
+                            </td>
+                            <td>
+                               Devis / Bon de commande 
+                            </td>
+                    </tr>
+
+                    ))}
+
+                  
+                </tbody>
+            </table>
         </div>
     );
 
