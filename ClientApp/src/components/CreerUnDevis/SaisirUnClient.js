8bbba84988671af1a74b13638d043fad8b427ba7
safiylm <121391404+safiylm@users.noreply.github.com>
2023-11-08 22:17:42 +0100

    form post add client

--- ClientApp/src/components/CreerUnDevis/SaisirUnClient.js
+++ ClientApp/src/components/CreerUnDevis/SaisirUnClient.js
@@ -1,23 +1,15 @@
 ﻿import React, { Component, useState } from 'react';
-import PorgressBarCreationDevis from './PorgressBarCreationDevis'
 import '../../styles/formCreationDevis.css';
 
 const SaisirUnClient  = () => {
 
    
-    const [client, setClient] = useState({ id : 9, 
-        nom: "hadi", prenom: "zhadiii", email: "safinaz@gmail.com",
-        adresse: "8 rue Jean Pascal", codePostale: 54300, ville: "Nancy", telephone: 444 , dateCreation
-            : Date.now, devisId : 1 });
-  
-    const [Nom, setNom] = useState("Yilmaz");
-    const [Prenom, setPrenom] = useState("Safinaz");
-    const [Email, setEmail] = useState("safinaz@gmail.com");
-    const [Adresse, setAdresse] = useState("9 rue casablanca");
-    const [CodePostale, setCodePostale] = useState("89000");
-    const [Ville, setVille] = useState("Lyon");
-    const [Telephone, setTelephone] = useState("06 06 06 06 05");
-    const [Message, setMessage] = useState("06 06 06 06 05");
+
+    const [client, setClient] = useState({
+        nom: "Nom", prenom: "Prenom", email: "Email",
+        adresse: "Adresse", codePostale: "CodePostale", ville: "Ville",
+        telephone: "Telephone", dateCreation : Date.now
+    });
 
 
     const handleClick = (event) => {
@@ -47,33 +39,31 @@ const SaisirUnClient  = () => {
                    if (request.readyState == 4 && request.status == 200) {
                       var response = JSON.parse(request.responseText);
                        console.log(response);
-                      alert("Hello: " + response.prenom + " .\n et le reste : " + response);
-                       setMessage("Hello: " + response.Name + " .\nCurrent Date and Time: " + response.DateTime);
                    }
                }.bind(this);
-              // request.send(JSON.stringify(client));
                request.send(formData);
          }
-        alert('A name was submitted: ' + Nom + ' ' + Prenom + " " + Email + " " + Adresse + " " + CodePostale + " " + Ville + " " + Telephone);
      }
 
-  
+    const changeHandler = (e) => {
+        setClient({ ...client, [e.target.name]: e.target.value })
+    }
    
     return (
         <div className="formCreationDevis">
           
             <form onSubmit={handleClick}>
-                <input type="text" className="form-control" name="nom" onChange={(e)=>setNom(e.target.value)} placeholder="Nom" />
-                <input type="text" className="form-control" name="prenom" onChange={(e) => setPrenom(e.target.value)} placeholder="Prenom" />
-                <input type="email" className="form-control" refs="Email" onChange={(e) => setEmail(e.target.value)} placeholder="Adresse Email" />
-                <input type="text" className="form-control" refs="Adresse" onChange={(e) => setAdresse(e.target.value)} placeholder="Adresse" />
-                <input refs="CodePostale" className="form-control" onChange={(e) => setCodePostale(e.target.value)} placeholder="CodePostale" />
-                <input type="text" className="form-control" refs="Ville" onChange={(e) => setVille(e.target.value)} placeholder="Ville" />
-                <input refs="Telephone" className="form-control" onChange={(e) => setTelephone(e.target.value)} placeholder="Telephone" />
-                <input refs="DevisId" type="hidden" value="1" />
+                <input type="text" className="form-control" name="nom" onChange={changeHandler } placeholder="Nom" />
+                <input type="text" className="form-control" name="prenom" onChange={changeHandler } placeholder="Prenom" />
+                <input type="email" className="form-control" name="email" onChange={changeHandler } placeholder="Adresse Email" />
+                <input type="text" className="form-control" name="adresse" onChange={changeHandler} placeholder="Adresse" />
+                <input className="form-control" name="codePostale" onChange={changeHandler} placeholder="CodePostale" />
+                <input type="text" className="form-control" name="ville" onChange={changeHandler} placeholder="Ville" />
+                <input className="form-control" name="telephone"  onChange={changeHandler} placeholder="Telephone" />
+
                 <button className="btn btn-primary" type="submit">Créer </button>
             </form>
-            {Message}
+           
         </div>
     );
     }
