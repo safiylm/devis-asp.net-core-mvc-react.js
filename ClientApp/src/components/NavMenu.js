35e006ceb74855ae2a0d93261373e2701eb8b3c0
safiylm <121391404+safiylm@users.noreply.github.com>
2024-01-15 17:36:42 +0100

    deplace css file in styles repertory

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import '../styles/NavMenu.css';
 import Devis from './Devis/Devis';
 import {
     BrowserRouter as Router,
@@ -36,10 +36,7 @@ export class NavMenu extends Component {
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
-              <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-
+             
              <NavItem>
                <NavLink tag={Link} className="text-dark" to="/Auth/Connexion">Connexion</NavLink>
              </NavItem>
