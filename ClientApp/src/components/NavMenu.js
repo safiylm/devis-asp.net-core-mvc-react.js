92124d9f1bccdfa89c6682957f7858f2a552eb7c
safiylm <121391404+safiylm@users.noreply.github.com>
2024-01-17 15:35:29 +0100

    Update NavMenu.js

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/NavMenu.css';
-import Devis from './Devis/Devis';
-import {
-    BrowserRouter as Router,
-    Route,
-    Routes,
-} from 'react-router-dom';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -29,34 +24,29 @@ export class NavMenu extends Component {
 
   render() {
       return (
-        <div>
+       
       <header>
-                  <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/"> Devis </NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-             
-             <NavItem>
-               <NavLink tag={Link} className="text-dark" to="/Auth/Connexion">Connexion</NavLink>
-             </NavItem>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+                  <NavbarBrand tag={Link} to="/"> Devis </NavbarBrand>
+
+                  <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                  <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                      <ul className="navbar-nav flex-grow">
+
+                         <NavItem>
+                           <NavLink tag={Link} className="text-dark" to="/Auth/Connexion">Connexion</NavLink>
+                         </NavItem>
+
+                          <NavItem>
+                           <NavLink tag={Link} className="text-dark" to="/ListeDevis">Mes factures </NavLink>
+                          </NavItem>
 
 
-            <NavItem>
-               <NavLink tag={Link} className="text-dark" to="/ListeDevis">Liste de facture </NavLink>
-            </NavItem>
-                       
-
-            </ul>
-          </Collapse>
-        </Navbar>
-              </header>
-
-              <Routes>
-                  <Route path="/Devis" element={<Devis />} />
-              </Routes>
-
-          </div>
+                     </ul>          
+                  </Collapse>
+            </Navbar>
+                
+       </header>
 
     );
   }
