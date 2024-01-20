1a733f8fabaf1cd1e46724599217df72beff3cb5
safiylm <121391404+safiylm@users.noreply.github.com>
2024-01-20 20:41:23 +0100

    get url param

--- ClientApp/src/components/CreerUnDevis/Index.js
+++ ClientApp/src/components/CreerUnDevis/Index.js
@@ -1,12 +1,13 @@
-﻿import React, { useState } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import PorgressBarCreationDevis from './PorgressBarCreationDevis';
 import SaisirUnClient from './Form/SaisirUnClient';
 import SaisirUneEntreprise from './Form/SaisirUneEntreprise';
 import SaisirLesProduits from './Form/SaisirLesProduits';
 import SaisirLesDerniersInformations from './Form/SaisirLesDerniersInformations';
+import { useSearchParams, useLocation }  from "react-router-dom"
+
 
 function Index() {
-
     const [idEntreprise, setIdEntreprise] = useState(0)
     const [emailEntreprise, setEmailEntreprise] = useState()
     const [dateCreationEntreprise, setDateCreationEntreprise] = useState()
@@ -16,16 +17,14 @@ function Index() {
     const [dateCreationClient, setDateCreationClient] = useState()
 
     const [numEtapee, changeNumEtape] = useState(1)
-    const queryParameters = new URLSearchParams(window.location.search)
-    const id_param = queryParameters.get("id")
-
-    const [tvaTotal, setTvaTotal] = useState(0)
-    const [htTotal, setHtTotal] = useState(0)
-
+ 
+    const url_ = window.location.pathname;
+    const arrayURL = url_.split("/");
+    const id_param = arrayURL[2];
 
     return (
         <div className="page-formCreationDevis">
-                Id = {id_param }<br/>
+            Id = {id_param}<br />
                 Entreprise = {idEntreprise}{emailEntreprise}{dateCreationEntreprise}<br />
                 Client = {idClient}{emailClient}{dateCreationClient}
                 <PorgressBarCreationDevis numEtapee={numEtapee} changeNumEtape={changeNumEtape } />
@@ -33,7 +32,7 @@ function Index() {
                 {numEtapee == 1 && < SaisirUneEntreprise setIdEntreprise={setIdEntreprise} setDateCreationEntreprise={setDateCreationEntreprise} setEmailEntreprise={setEmailEntreprise} changeNumEtape={changeNumEtape} />}
                 {numEtapee == 2 && < SaisirUnClient setIdClient={setIdClient} setEmailClient={setEmailClient} setDateCreationClient={setDateCreationClient} changeNumEtape={changeNumEtape} />}
                 {numEtapee == 3 && < SaisirLesProduits idDevis={id_param} changeNumEtape={changeNumEtape} />}
-                {numEtapee == 4 && < SaisirLesDerniersInformations idDevis={id_param} changeNumEtape={changeNumEtape} idClient={idClient} idEntreprise={idEntreprise} tvaTotal={tvaTotal} htTotal={htTotal } /> }
+                {numEtapee == 4 && < SaisirLesDerniersInformations idDevis={id_param} changeNumEtape={changeNumEtape} idClient={idClient} idEntreprise={idEntreprise} /> }
         </div>
         );
     
