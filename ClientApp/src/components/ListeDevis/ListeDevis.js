955b4e74481f4ce784f28a7190319ef1daf1277e
safiylm <121391404+safiylm@users.noreply.github.com>
2024-01-24 19:54:56 +0100

    update frontend

--- ClientApp/src/components/ListeDevis/ListeDevis.js
+++ ClientApp/src/components/ListeDevis/ListeDevis.js
@@ -44,13 +44,13 @@ const ListeDevis = () => {
         <div className="page-liste-devis">
         <div className="div-search-">
                 <h1>Mes Factures</h1>
-                <input className="form-control" placeholder="Recherhcez votre devis" />
-                <button className="btn btn-success">Rechercher</button>
-                <button className="btn btn-success" onClick={() => setShow(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" color="white" fill="currentColor" className="bi bi-plus-square-fill" viewBox="0 0 16 16">
+                <input className="form-control" placeholder="Recherhcez votre devis" style={{ width: "40vw" }} />
+                <button className="btn btn-light">Rechercher</button>
+                <button className="btn btn-light" onClick={() => setShow(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" color="black" fill="currentColor" className="bi bi-plus-square-fill" viewBox="0 0 16 16">
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0" />
-                </svg> nouveau devis</button>
-                <button className="btn btn-success" onClick={hrefDataUsers }> <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person-square" viewBox="0 0 16 16">
+                </svg> Nouveau devis</button>
+                <button className="btn btn-light" onClick={hrefDataUsers }> <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" className="bi bi-person-square" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
                 </svg> </button>
